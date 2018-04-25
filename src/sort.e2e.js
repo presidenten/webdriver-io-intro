5e0c67ecea4baddd4b726971cb863c5c91ec4b05
Johan Hernefeldt <johan.hernefeldt@teliacompany.com>
2018-04-25 09:08:17 +0200

    Initial commit

---
+++ src/sort.e2e.js
@@ -0,0 +1,11 @@
+describe('User list', () => {
+  beforeEach(() => {
+    browser.url('localhost:8080');
+  });
+
+  it('should be able to sort', () => {
+    // Write this test
+    expect(false).toBe(true); // Remove this line
+  });
+});
+
